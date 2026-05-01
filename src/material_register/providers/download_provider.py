fea4679d1ab30c38dbdd7f86c0d6a435229e5764
Jin-Mach <ji82ma@seznam.cz>
2026-05-01 20:09:55 +0200

    Add error_texts.json and URLs to DownloadProvider.FILES_MAP.

--- src/material_register/providers/download_provider.py
+++ src/material_register/providers/download_provider.py
@@ -11,6 +11,8 @@ class DownloadProvider:
     FILES_MAP = {
         "texts/cs_CZ/ui_texts.json": "https://raw.githubusercontent.com/Jin-Mach/material-register/main/resources/texts/cs_CZ/ui_texts.json",
         "texts/en_GB/ui_texts.json": "https://raw.githubusercontent.com/Jin-Mach/material-register/main/resources/texts/en_GB/ui_texts.json",
+        "texts/cs_CZ/error_texts.json": "https://raw.githubusercontent.com/Jin-Mach/material-register/main/resources/texts/cs_CZ/error_texts.json",
+        "texts/en_GB/error_texts.json": "https://raw.githubusercontent.com/Jin-Mach/material-register/main/resources/texts/en_GB/error_texts.json"
     }
 
     FILES_SUFFIXES = [".json", ".qss"]
