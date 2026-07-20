a4e544d3af67c866f501a728bdbd6b3aa7864e2b
Jin-Mach <ji82ma@seznam.cz>
2026-07-20 19:32:32 +0200

    Fix and update format_datetime_to_locale to display correct datetime format.

--- src/material_register/ui/helpers/formating_utils.py
+++ src/material_register/ui/helpers/formating_utils.py
@@ -1,11 +1,15 @@
 from PySide6.QtCore import QLocale, Qt, QDateTime
 
 
-_locale = QLocale.system()
+DATE_FORMAT = "yyyy-MM-dd HH:mm:ss"
+
+_locale = QLocale()
 
 def format_number_to_locale(number: float) -> str:
     return _locale.toString(number, "f", 1)
 
 def format_datetime_to_locale(iso_datetime: str) -> str:
-    dt = QDateTime.fromString(iso_datetime, Qt.ISODate)
-    return _locale.toString(dt, QLocale.FormatType.ShortFormat)
+    date_time = QDateTime.fromString(iso_datetime, DATE_FORMAT)
+    date_time.setTimeSpec(Qt.TimeSpec.UTC)
+    date_time = date_time.toLocalTime()
+    return _locale.toString(date_time, QLocale.FormatType.ShortFormat)
