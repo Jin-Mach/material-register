c16f6324c63b52345c6b94f76fb505d8feffa11e
Jin-Mach <ji82ma@seznam.cz>
2026-09-05 15:09:12 +0200

    Limit consecutive line breaks in text edits.

--- src/material_register/ui/helpers/notes_length_handler.py
+++ src/material_register/ui/helpers/notes_length_handler.py
@@ -7,15 +7,18 @@ def check_notes_length(
     notes_widget: QTextEdit, notes_label: QLabel, notes_text: str, notes_length: int
 ) -> None:
     text = notes_widget.toPlainText()
+    while "\n\n\n" in text:
+        text = text.replace("\n\n\n", "\n\n")
     if len(text) > notes_length:
         notes_widget.setStyleSheet(INVALID_INPUT_STYLE)
         text = text[:notes_length]
+    else:
+        notes_widget.setStyleSheet("")
+    if text != notes_widget.toPlainText():
         notes_widget.blockSignals(True)
         notes_widget.setPlainText(text)
         notes_widget.blockSignals(False)
         cursor = notes_widget.textCursor()
         cursor.movePosition(cursor.MoveOperation.End)
         notes_widget.setTextCursor(cursor)
-    else:
-        notes_widget.setStyleSheet("")
     notes_label.setText(f"{notes_text} {len(text)}/{notes_length}")
